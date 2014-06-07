d8d986672c062dc509ad03fc9555b4c8dfd36968
pydev <pydev@mailinator.com>
2014-06-07 20:50:09 +1200

    fixed moving off grid

--- test/test_world.py
+++ test/test_world.py
@@ -17,4 +17,5 @@ class TestWorld(TestCase):
         self.assertEqual(world.arena.findcharacter('asdf').gettuple(), Point(1, 5).gettuple())
         world.move('item', 'right')
         self.assertEqual(world.arena.findcharacter('item').gettuple(), Point(0, 6).gettuple())
-        
+        world.move('item', 'up')
+        self.assertEqual(world.arena.findcharacter('item').gettuple(), Point(0, 6).gettuple())
