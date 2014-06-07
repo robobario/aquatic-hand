d9f78f46d3670af8d42fcee8558bd647cbc6578d
pydev <pydev@mailinator.com>
2014-06-07 20:35:37 +1200

    test case for world.move

--- test/test_world.py
+++ test/test_world.py
@@ -11,4 +11,6 @@ __author__ = 'python'
 class TestWorld(TestCase):
     def test_move(self):
         world = World()
-        world.arena.getlocation(Point(1, 5)).
+        world.arena.getlocation(Point(1, 5)).additem('asdf')
+        world.move('asdf', 'up')
+        world.arena.findcharacter('asdf')
