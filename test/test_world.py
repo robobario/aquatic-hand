d80688f4b07cc4cb393ffb593a64b7b8a8524140
pydev <pydev@mailinator.com>
2014-06-07 21:31:44 +1200

    added attack

--- test/test_world.py
+++ test/test_world.py
@@ -1,4 +1,7 @@
 from unittest import TestCase
+from npc import Npc
+
+from pc import Pc
 
 from spacial import Point
 
@@ -13,9 +16,22 @@ class TestWorld(TestCase):
         world = World()
         world.arena.getlocation(Point(1, 5)).additem('asdf')
         world.arena.getlocation(Point(0, 5)).additem('item')
-        world.move('asdf', 'up')
-        self.assertEqual(world.arena.findcharacter('asdf').gettuple(), Point(1, 5).gettuple())
         world.move('item', 'right')
         self.assertEqual(world.arena.findcharacter('item').gettuple(), Point(0, 6).gettuple())
         world.move('item', 'up')
         self.assertEqual(world.arena.findcharacter('item').gettuple(), Point(0, 6).gettuple())
+
+    def test_moveattack(self):
+        world = World()
+        man = Pc()
+        world.spawn(man)
+        cat = Npc('kitty')
+        world.arena.getlocation(Point(0, 1)).additem(cat)
+        cathp = world.arena.getlocation(Point(0, 1)).characters[0].hp
+        expected = 100
+        self.assertEqual(cathp, expected)
+        world.move(man, 'right')
+        self.assertEqual(world.arena.findcharacter(man).gettuple(), (0, 0))
+        cathp = world.arena.getlocation(Point(0, 1)).characters[0].hp
+        expected = 90
+        self.assertEqual(cathp, expected)
